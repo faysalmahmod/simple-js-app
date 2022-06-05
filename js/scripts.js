91feb7f375603bc7a8be69505218650b8f8f8003
Faisal mahmood <faysalmahmod7@gmail.com>
2022-06-05 16:14:18 +0200

    Update scripts.js
    For replaced by foreach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 
-  Let pokemonList=[
+  let pokemonList=[
       {name: `Bulbasur`, height: `0.7`, types: [`grass`, `poison`, `flying`, `electric`]},
       {name: `Jigglypuff`, height: `0.2`, types: [`steel`, `poison`]},
       {name: `Pikachu`, height: `0.9`, types: [`ground`, `flying`, `steel`]},
@@ -7,7 +7,7 @@
       {name: `Starmie`, height: `1.7`, types: [`dragon`, `ghost`, `bug`, `fire`, `ice`]}
     ];
 
-    for (let i = 0; i < pokemonList.length; i++) {
+  /*   for (let i = 0; i < pokemonList.length; i++) {
       document.write('The name of Pokemon is ' + pokemonList[i].name + ' ' + 'and height is ' + pokemonList[i].height);
 
       if(pokemonList[i].height >= 1.7) {
@@ -18,3 +18,18 @@
           document.write(`small in size. ` + `<br>`);
       }
     }
+    */
+
+
+      pokemonList.forEach(function(pokemon) {
+          document.write('The name of Pokemon is ' + pokemon.name + ' ' + 'and height is ' + pokemon.height);
+
+          if(pokemon.height >= 1.7) {
+              document.write(`Wow, that\`s big!`+ `<br>`);
+          } else if(pokemon.height >=0.5 && pokemon.height <=1.1) {
+              document.write(`medium size. `+ `<br>`);
+          } else {
+              document.write(`small in size. ` + `<br>`);
+          }
+
+        });
